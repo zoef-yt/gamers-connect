8b74cea07f86279402282b75c9165c81500ceb57
Zoef <shaikhzoef36@gmail.com>
2022-05-12 11:32:09 +0530

    FIX: sidebar and page not found

--- src/Pages/PageNotFound/PageNotFound.jsx
+++ src/Pages/PageNotFound/PageNotFound.jsx
@@ -4,7 +4,7 @@ import './PageNotFound.css';
 const PageNotFound = () => {
 	const navigate = useNavigate();
 	return (
-		<div className='app page-not-found'>
+		<div className='app-content page-not-found'>
 			<h1 className='text-404'>404</h1>
 			<h1> Oh! How did you get here? Nothing to see here</h1>
 			<button onClick={() => navigate('/')} className='btn btn-primary'>
